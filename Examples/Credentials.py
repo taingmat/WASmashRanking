bbced9ba0dce25e88918291e6468525eb3f6f463
taingmat <taingmat@uw.edu>
2019-10-10 14:47:49 -0700

    got mysql server running and added example usage

---
+++ Examples/Credentials.py
@@ -0,0 +1,5 @@
+def get_user():
+    return 'mika'
+
+def get_password():
+    return "@24c0FF334TW0"
---
+++ Examples/MySQL_Example.py
@@ -0,0 +1,28 @@
+import pymysql
+import Credentials
+
+# Open database connection
+
+db = pymysql.connect(host = "192.168.1.105",
+                     port = 3306,
+                     db = "WASMASH",
+                     user = Credentials.get_user(),
+                     passwd = Credentials.get_password())
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print ("Database version : %s " % data)
+
+cursor.execute("SHOW TABLES")
+
+data = cursor.fetchall()
+
+print(data)
+# disconnect from server
+db.close()
